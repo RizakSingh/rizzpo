9165b67a34cede411e3ae46844d844fd1b7e705e
RizakSingh <srizak95@gmail.com>
2026-04-07 17:24:04 +0530

    created tab routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,68 @@
+import { clsx } from "clsx";
+import { Image } from "react-native";
 import { Tabs } from "expo-router";
+import { View } from "react-native";
 import { tabs } from "../../constants/data";
-const Tablayout = ()=>(
+import{ colors,components} from "../../constants/theme";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+const tabBar = components.tabBar
+const Tablayout = () => {
+  const insets = useSafeAreaInsets();
+ const TabIcon = ({ focused, icon }: TabIconProps) => {
+  return (
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <View
+        style={{
+          width: 44,
+          height: 44,
+          borderRadius: 22,
+          backgroundColor: focused ? "#FF7A00" : "transparent",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image source={icon} style={{ width: 22, height: 22 }} resizeMode="contain" />
+      </View>
+    </View>
+  );
+};
 
-  <Tabs screenOptions={{ headerShown: false }}>
-    {tabs.map((tab) => (
-      <Tabs.Screen
-        key={tab.name}
-        name={tab.name}
-        options={{ title: tab.title, tabBarIcon: tab.icon }}
-      />
-    ))}
-  </Tabs>
-)
-export default Tablayout 
+  return (
+    <Tabs
+    screenOptions={{
+  headerShown: false,
+  tabBarShowLabel: false,
+  tabBarStyle: {
+    position: 'absolute',
+    bottom: Math.max(insets.bottom, tabBar.horizontalInset),
+    height: tabBar.height,
+    marginHorizontal: tabBar.horizontalInset,
+    borderRadius: tabBar.radius,
+    backgroundColor: colors.primary,
+    borderTopWidth: 0,
+    elevation: 0
+  },
+  tabBarItemStyle: {
+    paddingVertical: tabBar.height/2 - tabBar.iconFrame/2.5,
+    width: tabBar.iconFrame,
+    height: tabBar.iconFrame,
+    alignItems: "center",
+  },
+}}>
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={tab.icon} />
+            ),
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+};
+
+export default Tablayout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,17 @@
 import { Link } from "expo-router";
 import "@/global.css"
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
  
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <SafeAreaView className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
       <Link href="/(tabs)/Insigths">
         <Text className="text-blue-500 underline">View Subscription</Text>
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
